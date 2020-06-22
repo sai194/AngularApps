99a4353f1d01ce14f9c8c930b9e03b9cfc9a1c81
Sai Yeluri <sai194@gmail.com>
2020-06-22 08:20:31 -0500

    Add Form group

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -8,9 +8,27 @@ import {FormControl, FormGroup} from "@angular/forms";
 })
 export class AppComponent {
   title = 'reactive-forms';
-  registrationForm  = new FormGroup({
+  registrationForm = new FormGroup({
     userName: new FormControl('Sai'),
     password: new FormControl(''),
     confirmPassword: new FormControl(''),
+    address: new FormGroup({
+      city: new FormControl(''),
+      state: new FormControl(''),
+      postalCode: new FormControl('')
+    })
   });
+
+  loadAPIData() {
+    this.registrationForm.patchValue({
+      userName: 'Bruce',
+      password: 'test',
+      //confirmPassword: 'test',
+      address: {
+        city: 'City',
+        state: 'State',
+        postalCode: '123456'
+      }
+    });
+  }
 }
